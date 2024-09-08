6b6d6a928501d9de567e2768c5d8d78359b683e4
Lemachipascal-dev <lemachijo421@gmail.com>
2024-09-08 12:43:17 +0300

    python-p3-countdown-to-midnight-lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,18 @@
 # your code goes here!
+def countdown(start):
+    while start > 0:
+        print(f'{start} SECOND(S)!')
+        start -= 1
+    print('HAPPY NEW YEAR!')
+
+print(countdown(10))
+
+import time
+
+def countdown_with_sleep(start):
+    while start > 0:
+        print(f'{start} SECOND(S)!')
+        time.sleep(1)  # Sleep for 1 second
+        start -= 1
+    print('HAPPY NEW YEAR!')
+print(countdown_with_sleep(10))
